2466939d5e6c22bc9beaceff32d170257686cc17
jonnystv <jonathan@darefoundation.com>
2021-02-25 11:47:01 +0000

    Java models created

--- src/main/java/com/codeclan/example/course_booking/models/Course.java
+++ src/main/java/com/codeclan/example/course_booking/models/Course.java
@@ -1,4 +1,64 @@
 package com.codeclan.example.course_booking.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+
+    private Long id;
+    private String name;
+    private String town;
+    private int rating;
+    private List<Booking> bookings;
+
+    public Course(String name, String town, int rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+        this.bookings = new ArrayList<Booking>;
+    }
+
+    public Long getCourseId() {
+        return id;
+    }
+
+    public void setCourseId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return name;
+    }
+
+    public void setCourseName(String name) {
+        this.name = name;
+    }
+
+    public String getCourseTown() {
+        return town;
+    }
+
+    public void setCourseTown(String town) {
+        this.town = town;
+    }
+
+    public int getCourseRating() {
+        return rating;
+    }
+
+    public void setCourseRating(int rating) {
+        this.rating = rating;
+    }
+
+    public List<Booking> getCourseBookings() {
+        return bookings;
+    }
+
+    public void setCourseBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
 }
--- src/main/java/com/codeclan/example/course_booking/models/Customer.java
+++ src/main/java/com/codeclan/example/course_booking/models/Customer.java
@@ -1,4 +1,62 @@
 package com.codeclan.example.course_booking.models;
 
+import java.util.List;
+
 public class Customer {
+
+    private Long id;
+    private String name;
+    private String town;
+    private int age;
+    private List<Booking> bookings;
+
+    public Customer(String name, String town, int age) {
+        this.name = name;
+        this.town = town;
+        this.age = age;
+    }
+
+    public Long getCustomerId() {
+        return id;
+    }
+
+    public void setCustomerId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return name;
+    }
+
+    public void setCustomerName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomerTown() {
+        return town;
+    }
+
+    public void setCustomerTown(String town) {
+        this.town = town;
+    }
+
+    public int getCustomerAge() {
+        return age;
+    }
+
+    public void setCustomerAge(int age) {
+        this.age = age;
+    }
+
+    public List<Booking> getCustomerBookings() {
+        return bookings;
+    }
+
+    public void setCustomerBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void addBooking(Booking booking) {
+        this.bookings.add(booking);
+    }
 }
