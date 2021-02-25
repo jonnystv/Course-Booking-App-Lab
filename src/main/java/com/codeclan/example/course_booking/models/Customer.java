04d674855bf58889d4fe8748c74308474cf268e6
jonnystv <jonathan@darefoundation.com>
2021-02-25 12:06:51 +0000

    Customer model annotated

--- src/main/java/com/codeclan/example/course_booking/models/Customer.java
+++ src/main/java/com/codeclan/example/course_booking/models/Customer.java
@@ -1,13 +1,29 @@
 package com.codeclan.example.course_booking.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "customers")
 public class Customer {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "town")
     private String town;
+
+    @Column(name = "age")
     private int age;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"customer"})
     private List<Booking> bookings;
 
     public Customer(String name, String town, int age) {
